6c0f20ca3599e773b1248c07b0826cbbe7d58f48
YAEGASHI Takeshi <yaegashi@gmail.com>
2020-06-29 21:03:01 +0900

    Update variable names and docs

--- cmder.go
+++ cmder.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// Cmder is an interface for objects that return cobra.Command
+// Cmder is the interface for objects that form a command hierarchy
 type Cmder interface {
 	Cmd() *cobra.Command
 }
@@ -18,41 +18,38 @@ type Cmder interface {
 // Cmd traverses a Cmder hierarchy and returns a configured cobra.Command.
 // It recursively calls all methods that return a Cmder
 // to collect and associate cobra.Command instances.
-func Cmd(cmder Cmder) *cobra.Command {
-	return recCmd(cmder, map[string]bool{})
+func Cmd(c Cmder) *cobra.Command {
+	return recCmd(c, map[string]bool{})
 }
 
 // outTypes is a constant for the array of function output types
 var outTypes = []reflect.Type{reflect.TypeOf((*Cmder)(nil)).Elem()}
 
-// recCmd is an actual worker function to visit and collect Cmder instances.
-// methodMap is for bookkeeping already visted method names.
-func recCmd(cmder Cmder, methodMap map[string]bool) *cobra.Command {
-	cmd := cmder.Cmd()
-	inV := reflect.ValueOf(cmder)
-	inT := reflect.TypeOf(cmder)
+// recCmd is the actual worker function to visit and collect Cmder instances.
+// mmap is for bookkeeping already visted method names.
+func recCmd(c Cmder, mmap map[string]bool) *cobra.Command {
+	cmd := c.Cmd()
+	inV := reflect.ValueOf(c)
+	inT := reflect.TypeOf(c)
 	funcT := reflect.FuncOf([]reflect.Type{inT}, outTypes, false)
-	methodList := []reflect.Method{}
+	methods := []reflect.Method{}
 	for i := 0; i < inT.NumMethod(); i++ {
 		m := inT.Method(i)
-		if m.Func.Type() != funcT {
+		if m.Func.Type() != funcT || mmap[m.Name] {
 			continue
 		}
-		if methodMap[m.Name] {
-			continue
-		}
-		methodList = append(methodList, m)
+		methods = append(methods, m)
 	}
-	for _, m := range methodList {
-		methodMap[m.Name] = true
+	for _, m := range methods {
+		mmap[m.Name] = true
 	}
-	for _, m := range methodList {
-		subCmder := m.Func.Call([]reflect.Value{inV})[0].Interface().(Cmder)
-		subCmd := recCmd(subCmder, methodMap)
+	for _, m := range methods {
+		subC := m.Func.Call([]reflect.Value{inV})[0].Interface().(Cmder)
+		subCmd := recCmd(subC, mmap)
 		cmd.AddCommand(subCmd)
 	}
-	for _, m := range methodList {
-		methodMap[m.Name] = false
+	for _, m := range methods {
+		mmap[m.Name] = false
 	}
 	return cmd
 }
